84a54538562ef8aafdfaf2eb9c43f65cc5833570
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-03-07 10:40:22 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,49 @@
-function dijkstra(graph, sourceNode) {
-    return [];
+const graph = {
+    A: { B: 1, C: 4 },      
+    B: { A: 1, C: 2, D: 5 },  
+    C: { A: 4, B: 2, D: 1 },
+    D: { B: 5, C: 1 }
+};
+
+function dijkstra(graph, sourceNode)  {
+    if(graph.length === 0)
+
+    var distances = {};
+    var nodes = Object.keys(graph);
+    var visited = new Set(Object.keys(graph));
+
+    for (var node of nodes) {
+        distances[node] = Infinity;
+    }
+
+    distances[sourceNode] = 0;
+
+    while (visited.size > 0) {
+        var min = Infinity;
+        var nextNode = null;
+
+        for (var node of nodes) {
+            if (visited.has(node) && distances[node] < min) {
+                min = distances[node];
+                nextNode = node;
+            }
+        }
+
+        if (nextNode === null) {
+            break;
+        }
+
+        visited.delete(nextNode);
+
+        var neighbors = Object.keys(graph[nextNode]);
+
+        for (var neighbor of neighbors) {
+            var newDistance = distances[nextNode] + graph[nextNode][neighbor];
+            if (newDistance < distances[neighbor]) {
+                distances[neighbor] = newDistance;
+            }
+        }
+    }
+
+    return distances;
 }
